25a4799ecc2836c51a81bc9ee3363ea3dcdbd556
nain93 <73378472+nain93@users.noreply.github.com>
2021-08-24 14:41:30 +0900

    Feat:#5 Add Task

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from 'components/Header';
+import TodoList from './TodoList';
+import styled from 'styled-components';
 
 const MainPage = () => {
   return (
-    <div>
+    <Container>
       <Header />
-    </div>
+      <TodoList />
+    </Container>
   );
 };
+
+const Container = styled.div`
+  max-width: 768px; 
+  margin:0 auto;
+
+`
 
 export default MainPage;
